88322ed18df322cbd7c80eda14116dcf36fcbbd1
Piotr Witek <pio.witek@gmail.com>
2021-12-06 15:23:38 +0100

    show only fusers firend

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friendsses
   #get 'home/index'
-  root 'home#index'
+  #root 'home#index'
+  root 'friendsses#index'
   get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
